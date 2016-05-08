309033f7afa8bead072ce39cb7c54b63b9e26168
mapserver2007 <mapserver2007@gmail.com>
2016-05-08 10:09:28 +0900

    一旦コミット

---
+++ IntegrationTest/ControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+namespace WebStream\Test\IntegrationTest;
+
+/**
+ * ControllerTest
+ * @author Ryuichi TANAKA.
+ * @since 2016/05/07
+ * @version 0.7
+ */
+class ControllerTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+    }
+
+    public function tearDown()
+    {
+    }
+
+    /**
+     * 正常系
+     * SELECT文を直接実行できること
+     * @test
+     */
+    public function okSelect()
+    {
+
+        $this->assertTrue(true);
+    }
+}
---
+++ IntegrationTest/TestApp/app/controllers/TestControllerController.php
@@ -0,0 +1,12 @@
+<?php
+namespace WebStream\Test\IntegrationTest\Controller;
+
+use WebStream\Core\CoreController;
+
+class TestControllerController extends CoreController
+{
+    public function test1()
+    {
+        echo "test1";
+    }
+}
