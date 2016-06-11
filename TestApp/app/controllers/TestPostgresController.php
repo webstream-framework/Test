e5053b54058c60005913896aff7c3f80e3165ffe
mapserver2007 <mapserver2007@gmail.com>
2016-06-11 09:58:48 +0900

    DB系テストケース追加

--- TestApp/app/controllers/TestPostgresController.php
+++ TestApp/app/controllers/TestPostgresController.php
@@ -63,6 +63,91 @@ class TestPostgresController extends CoreController
         echo count($this->TestPostgres->pop());
     }
 
+
+    public function entityMapping1()
+    {
+        $this->TestPostgres->cleanUp();
+        $this->TestPostgres->setUp("dummy");
+        foreach ($this->TestPostgres->entityMapping1() as $entity) {
+            echo get_class($entity);
+        }
+    }
+
+    public function entityMapping2()
+    {
+        $this->TestPostgres->cleanUp();
+        foreach ($this->TestPostgres->entityMapping2() as $entity) {
+            echo get_class($entity);
+        }
+    }
+
+    public function entityMappingJoinedTable()
+    {
+        $this->TestPostgres->cleanUp();
+        foreach ($this->TestPostgres->entityMappingJoinedTable() as $entity) {
+            echo $entity->getName();
+            echo $entity->getValue1();
+            echo $entity->getValue2();
+            echo $entity->getValue3();
+        }
+    }
+
+    public function entityMappingColumnAlias()
+    {
+        $this->TestPostgres->cleanUp();
+        foreach ($this->TestPostgres->entityMappingColumnAlias() as $entity) {
+            echo $entity->getName1();
+            echo $entity->getName2();
+        }
+    }
+
+    public function entityMappingType()
+    {
+        $this->TestPostgres->cleanUp();
+        foreach ($this->TestPostgres->entityMappingType() as $entity) {
+            echo gettype($entity->getId());
+            echo gettype($entity->getName());
+            echo gettype($entity->getCreatedAt());
+            echo gettype($entity->getCreatedAtTime());
+            echo gettype($entity->getCreatedAtDate());
+            echo gettype($entity->getBigintNum());
+            echo gettype($entity->getSmallintNum());
+        }
+    }
+
+    public function entityMappingTrait()
+    {
+        $this->TestPostgres->cleanUp();
+        foreach ($this->TestPostgres->entityMappingTrait() as $entity) {
+            echo $entity->getName1();
+            echo $entity->getName2();
+        }
+    }
+
+    public function entityMappingPropertyProxy()
+    {
+        $this->TestPostgres->cleanUp();
+        foreach ($this->TestPostgres->entityMappingPropertyProxy() as $entity) {
+            echo $entity->name;
+        }
+    }
+
+    public function transactionCommit()
+    {
+        $this->TestPostgres->cleanUp();
+        $this->TestPostgres->transactionCommit();
+        $result = $this->TestPostgres->annotationSelectQuery();
+        echo $result->toArray()[0]["name"];
+    }
+
+    public function transactionRollback()
+    {
+        $this->TestPostgres->cleanUp();
+        $this->TestPostgres->transactionRollback();
+        $result = $this->TestPostgres->annotationSelectQuery();
+        echo count($result->toArray());
+    }
+
     public function print($result)
     {
         echo $result->toArray()[0]["name"];
--- TestApp/app/models/TestPostgresModel.php
+++ TestApp/app/models/TestPostgresModel.php
@@ -74,6 +74,100 @@ class TestPostgresModel extends CoreModel
     }
 
     /**
+     * @Query(file="query/webstream-test-entity-mapper.xml")
+     */
+    public function entityMapping1()
+    {
+        return $this->queryEntitySelect1();
+    }
+
+    /**
+     * @Query(file="query/webstream-test-entity-mapper.xml")
+     */
+    public function entityMapping2()
+    {
+        $this->queryEntityMappingDelete();
+        $this->queryEntityMappingInsert(['value1' => "a", 'value2' => "b", 'value3' => "c"]);
+
+        return $this->queryEntitySelect2();
+    }
+
+    /**
+     * @Query(file="query/webstream-test-common-mapper.xml")
+     * @Query(file="query/webstream-test-entity-mapper.xml")
+     */
+    public function entityMappingJoinedTable()
+    {
+        $this->querySetUp(['name' => 'a']);
+        $this->queryEntityMappingInsert(['value1' => "b", 'value2' => "c", 'value3' => "d"]);
+
+        return $this->queryEntityJoinedTable();
+    }
+
+    /**
+     * @Query(file="query/webstream-test-common-mapper.xml")
+     * @Query(file="query/webstream-test-entity-mapper.xml")
+     */
+    public function entityMappingColumnAlias()
+    {
+        $this->querySetUp(['name' => 'a']);
+
+        return $this->queryEntityColumnAlias();
+    }
+
+    /**
+     * @Query(file="query/webstream-test-entity-mapper.xml")
+     */
+    public function entityMappingType()
+    {
+        $this->queryEntityMappingTypeInsert(['name' => "a", 'bigint_num' => 9223372036854775807, 'smallint_num' => 3]);
+
+        return $this->queryEntityType();
+    }
+
+    /**
+     * @Query(file="query/webstream-test-postgres-mapper.xml")
+     * @Query(file="query/webstream-test-entity-mapper.xml")
+     */
+    public function entityMappingTrait()
+    {
+        $this->queryAnnotationInsert(["name" => 'a']);
+
+        return $this->queryEntityTrait();
+    }
+
+    /**
+     * @Query(file="query/webstream-test-postgres-mapper.xml")
+     * @Query(file="query/webstream-test-entity-mapper.xml")
+     */
+    public function entityMappingPropertyProxy()
+    {
+        $this->queryAnnotationInsert(["name" => 'a']);
+
+        return $this->queryEntityPropertyProxy();
+    }
+
+    /**
+     * @Query(file="query/webstream-test-postgres-mapper.xml")
+     */
+    public function transactionCommit()
+    {
+        $this->beginTransaction();
+        $this->queryAnnotationInsert(["name" => 'a']);
+        $this->commit();
+    }
+
+    /**
+     * @Query(file="query/webstream-test-postgres-mapper.xml")
+     */
+    public function transactionRollback()
+    {
+        $this->beginTransaction();
+        $this->queryAnnotationInsert(["name" => 'a']);
+        $this->rollback();
+    }
+
+    /**
      * @Query(file="query/webstream-test-common-mapper.xml")
      */
     public function setUp($name)
@@ -83,10 +177,13 @@ class TestPostgresModel extends CoreModel
 
     /**
      * @Query(file="query/webstream-test-common-mapper.xml")
+     * @Query(file="query/webstream-test-entity-mapper.xml")
      */
     public function cleanUp()
     {
         $this->queryCleanUp();
+        $this->queryEntityMappingDelete();
+        $this->queryEntityMappingTypeDelete();
     }
 
     /**
